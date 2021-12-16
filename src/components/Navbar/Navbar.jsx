52598611e11b0b6dc633b3bbc4d232d3404c8e33
Le <hieu.le@accenture.com>
2021-12-16 22:06:51 +0100

    feat: create navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.scss";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar" data-scroll-section>
+      <div>menu</div>
+      <div>Harry Vintage</div>
+      <div>cart</div>
+    </div>
+  );
+};
